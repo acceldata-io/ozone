9c4436223785b7de267bbe7ce5a2e0f0396cba9a
Dishti Kundra <136782345+dishtikundra@users.noreply.github.com>
2025-10-24 09:13:12 +0530

    Revert "ODP-3773 Update authFilterConfigurationPrefix method to setAuthFilterConfigurationPrefix as per ODP-2997|HADOOP-18666 (#21)" (#24)
    This reverts commit 76505e65314008527e1a18b413b5de559fc2c6b1.

--- hadoop-ozone/httpfsgateway/src/main/java/org/apache/ozone/fs/http/server/HttpFSServerWebServer.java
+++ hadoop-ozone/httpfsgateway/src/main/java/org/apache/ozone/fs/http/server/HttpFSServerWebServer.java
@@ -123,7 +123,7 @@ public class HttpFSServerWebServer {
         .setName(NAME)
         .setConf(conf)
         .setSSLConf(sslConf)
-        .setAuthFilterConfigurationPrefix(HttpFSAuthenticationFilter.CONF_PREFIX)
+        .authFilterConfigurationPrefix(HttpFSAuthenticationFilter.CONF_PREFIX)
         .setACL(new AccessControlList(conf.get(HTTP_ADMINS_KEY, " ")))
         .addEndpoint(endpoint)
         .build();

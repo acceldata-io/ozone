644ac5b6277ff2aa393a25d1dbd9f59454e0db74
Ko-dany <dahyun.ko@acceldata.io>
2026-08-07 12:11:45 -0400

    ODP-6879: ODP-6879: Bump Ranger to 2.9.0.3.4.3.0-1 and fix RangerServiceException status type

--- hadoop-ozone/multitenancy-ranger/src/main/java/org/apache/hadoop/ozone/om/multitenant/RangerClientMultiTenantAccessController.java
+++ hadoop-ozone/multitenancy-ranger/src/main/java/org/apache/hadoop/ozone/om/multitenant/RangerClientMultiTenantAccessController.java
@@ -25,7 +25,6 @@ import static org.apache.hadoop.ozone.om.OMConfigKeys.OZONE_RANGER_HTTPS_ADDRESS
 import static org.apache.hadoop.ozone.om.OMConfigKeys.OZONE_RANGER_SERVICE;
 import static org.apache.hadoop.security.UserGroupInformation.AuthenticationMethod;
 
-import com.sun.jersey.api.client.ClientResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -35,6 +34,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import javax.ws.rs.core.Response;
 import org.apache.hadoop.hdds.conf.ConfigurationSource;
 import org.apache.hadoop.ozone.OmUtils;
 import org.apache.hadoop.ozone.OzoneConsts;
@@ -153,7 +153,7 @@ public class RangerClientMultiTenantAccessController implements
    * @param rse RangerServiceException
    */
   private void decodeRSEStatusCodes(RangerServiceException rse) {
-    ClientResponse.Status status = rse.getStatus();
+    Response.Status status = rse.getStatus();
     if (status == null) {
       LOG.error("Request failure with no status provided.", rse);
     } else {
